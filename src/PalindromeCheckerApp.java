597606ace21b19e0a90f0a31f09f3738ce6935c3
rk1888-2090 <rk1888@srmist.edu.in>
2026-02-23 09:44:09 +0530

    PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,33 +5,4 @@ public class PalindromeCheckerApp {
         System.out.println("      PALINDROME CHECKER APP     ");
         System.out.println("=================================");
         System.out.println("Welcome to the Application!");
-    public static boolean isPalindrome(String str) {
-        str = str.replaceAll("\\s+", "").toLowerCase();
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println("The string is a palindrome.");
-        } else {
-            System.out.println("The string is not a palindrome.");
-        }
-
-        scanner.close();
-    }
 }
