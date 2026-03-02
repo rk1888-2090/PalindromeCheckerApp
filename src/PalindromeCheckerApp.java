28048627d31b27c90b9f7ecf75f713885075b21e
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 09:22:01 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,26 @@
 import java.util.*;
-class UseCase9PalindromeCheckerApp
+class UseCase10PalindromeCheckerApp
 {
- static boolean check(String s,int start,int end)
- {
-  if(start>=end) return true;
-  if(s.charAt(start)!=s.charAt(end)) return false;
-  return check(s,start+1,end-1);
- }
  public static void main(String[] args)
- { Scanner sc=new Scanner(System.in);
+ {
+  Scanner sc=new Scanner(System.in);
   System.out.println("enter string");
   String s=sc.nextLine();
   s=s.replaceAll("\\s+","").toLowerCase();
-  if(check(s,0,s.length()-1)) System.out.println("palindrome");
+  int i=0;
+  int j=s.length()-1;
+  boolean ok=true;
+  while(i<j)
+  {
+   if(s.charAt(i)!=s.charAt(j))
+   {
+    ok=false;
+    break;
+   }
+   i++;
+   j--;
+  }
+  if(ok) System.out.println("palindrome");
   else System.out.println("not palindrome");
  }
 }
