4e9d008ad4a6a817192a474543b7659838725c41
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 08:47:19 +0530

    PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,11 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        System.out.println("=================================");
         System.out.println("      PALINDROME CHECKER APP     ");
-        System.out.println("=================================");
-
         String word = "madam";
         String reversed = "";
-
         for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-
+            reversed += word.charAt(i);}
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
