7ecbe3d021a88c18e72f84d5bdacdc089d927faa
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 08:49:29 +0530

    PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,19 @@
 import java.util.Scanner;
-
 public class UseCase3PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=========================================");
-        System.out.println("        PALINDROME CHECKER APP");
-        System.out.println("=========================================");
-
+        System.out.println("PALINDROME CHECKER APP");
         System.out.print("Enter a string: ");
         String original = scanner.nextLine();
-
         String reversed = "";
-
-        // Reverse string using for loop
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
-
-        // Compare original and reversed
         if (original.equals(reversed)) {
-            System.out.println("Result: The given string is a Palindrome.");
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
+            System.out.println("The given string is NOT a Palindrome.");
         }
-
         scanner.close();
     }
 }
