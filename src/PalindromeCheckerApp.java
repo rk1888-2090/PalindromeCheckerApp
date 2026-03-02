1d2c05ce8f46d7e0c0b7a368a3b63a666d9a8503
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 08:55:28 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,50 +2,34 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.Queue;
 import java.util.LinkedList;
-
 public class UseCase6PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=========================================");
-        System.out.println("        PALINDROME CHECKER APP");
-        System.out.println("=========================================");
-
+        System.out.println("PALINDROME CHECKER APP");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
         Stack<Character> stack = new Stack<>();
         Queue<Character> queue = new LinkedList<>();
-
-        // Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
-            stack.push(ch);      // LIFO
-            queue.add(ch);       // FIFO
+            stack.push(ch);      
+            queue.add(ch);       
         }
 
         boolean isPalindrome = true;
-
-        // Compare dequeue (FIFO) with pop (LIFO)
         for (int i = 0; i < input.length(); i++) {
-
-            char fromQueue = queue.remove();  // Dequeue
-            char fromStack = stack.pop();     // Pop
-
+            char fromQueue = queue.remove();  
+            char fromStack = stack.pop();     
             if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
-
         if (isPalindrome) {
-            System.out.println("Result: The given string is a Palindrome.");
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
+            System.out.println("The given string is NOT a Palindrome.");
         }
-
         scanner.close();
     }
 }
