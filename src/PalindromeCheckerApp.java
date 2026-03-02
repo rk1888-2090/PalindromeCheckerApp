0f8b09f24ce91052e79ac97cc97f26778ff26ff6
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 09:10:25 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.*;
+class UseCase8PalindromeCheckerApp
+{
+    static class Node
+    {
+        char data;
+        Node next;
+        Node(char d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    static Node head=null;
+    static void add(char c)
+    {
+        Node n=new Node(c);
+        if(head==null)
+        {
+            head=n;
+            return;
+        }
+        Node t=head;
+        while(t.next!=null) t=t.next;
+        t.next=n;
+    }
+    static boolean check()
+    {
+        if(head==null||head.next==null) return true;
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node prev=null;
+        Node curr=slow;
+        Node next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node first=head;
+        Node second=prev;
+        while(second!=null)
+        {
+            if(first.data!=second.data) return false;
+            first=first.next;
+            second=second.next;
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter string");
+        String s=sc.nextLine();
+        s=s.replaceAll("\\s+","").toLowerCase();
+        for(int i=0;i<s.length();i++) add(s.charAt(i));
+        if(check()) System.out.println("palindrome");
+        else System.out.println("not palindrome");
     }
 }
