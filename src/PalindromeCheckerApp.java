13032b496a087e87645f7c48dbedbcd090bf49ea
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 08:51:26 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,43 +1,27 @@
 import java.util.Scanner;
-
 public class UseCase4PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=========================================");
-        System.out.println("        PALINDROME CHECKER APP");
-        System.out.println("=========================================");
-
+        System.out.println("PALINDROME CHECKER APP");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
-        // Convert string to character array
         char[] characters = input.toCharArray();
-
         int left = 0;
         int right = characters.length - 1;
         boolean isPalindrome = true;
-
-        // Two-pointer approach
         while (left < right) {
-
             if (characters[left] != characters[right]) {
                 isPalindrome = false;
                 break;
             }
-
             left++;
             right--;
         }
-
         if (isPalindrome) {
-            System.out.println("Result: The given string is a Palindrome.");
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
+            System.out.println("The given string is NOT a Palindrome.");
         }
-
         scanner.close();
     }
 }
