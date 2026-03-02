142fd0fac45627430bcfc5aafef08d61948046ef
rk1888-2090 <rk1888@srmist.edu.in>
2026-03-02 08:46:02 +0530

    PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,4 @@
 import java.util.Scanner;
-
 public class PalindromeCheckerApp {
-    System.out.println("=================================");
         System.out.println("      PALINDROME CHECKER APP     ");
-        System.out.println("=================================");
-        System.out.println("Welcome to the Application!");
 }
